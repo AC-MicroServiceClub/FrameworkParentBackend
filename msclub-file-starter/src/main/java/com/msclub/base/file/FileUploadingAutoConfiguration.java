d41873eb7bc5c724727caf11183f87b94e52fed7
xiaohui.c.liu <!qaz2wsx>
2018-09-13 08:56:58 +0800

    remove unused class

--- msclub-file-starter/src/main/java/com/msclub/base/file/FileUploadingAutoConfiguration.java
+++ msclub-file-starter/src/main/java/com/msclub/base/file/FileUploadingAutoConfiguration.java
@@ -1,10 +1,8 @@
 package com.msclub.base.file;
 
-import org.apache.http.client.HttpClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.multipart.MultipartFile;
 
 /**
  * Provides a {@link org.springframework.web.client.RestTemplate} configured with
